e685585b66bc0527c533e93c81705118c7fd075a
Aakashdeveloper <aakash.h@hcl.com>
2018-02-13 08:44:55 +0530

    A

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Employee} from './models/employee.model'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  languages = ["NodeJS","AngularJS","ReactJS"]
+  model = new Employee("Learntek","Angular",true,"male","NodeJS")
+
+  firstToUpper(value:string): void{
+    if(value.length>0)
+      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1)
+  }
 }
